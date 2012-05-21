b715a0fcd81a306528afbdc0f56afd9bcec41966
Luigi Maselli <grigio.org@gmail.com>
2012-05-21 16:31:58 +0200

    ruby 1.8.7 test ok

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,10 +1,10 @@
 module ProductsHelper
   def product_for_template(product)
     {
-      url: product_url(product),
-      title: product.title,
-      price: number_to_currency(product.price),
-      description: product.description
+      :url => product_url(product),
+      :title => product.title,
+      :price => number_to_currency(product.price),
+      :description => product.description
     }
   end
 end
